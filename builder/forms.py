e6a226fb55f786482db4704c5f055e0a1ef23378
SamTheKorean <bumsuyi@gmail.com>
2025-03-09 15:40:00 -0400

    update : change placeholder

--- builder/forms.py
+++ builder/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class InputForm(forms.Form):
-    input_sentence = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'tell me about your experience', 'rows': 1,}))
+    input_sentence = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Tell me about your experience, e.g., "I made a web service using Django."',
+                'rows': 1,
+            }
+        )
+    )
